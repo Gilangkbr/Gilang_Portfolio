3d50cc676c25f637be8ee9434345dd2d2761b415
Gilang Ananda Akbar <gilangkbr4@gmail.com>
2026-08-17 19:35:52 +0700

    update profile photo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import "./header.css";
 import CTA from "./CTA";
-import profilePict from "../../assets/profile-nando.jpg";
+import profilePict from "../../assets/profile-nando-2.jpg";
 import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
